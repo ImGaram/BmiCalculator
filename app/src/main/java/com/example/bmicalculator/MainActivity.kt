190ad5b6ea8a159470008b20b4959a0dbb768b87
imgaram <s21049@gsm.hs.kr>
2022-02-17 20:48:44 +0900

    ui 수정

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -3,16 +3,21 @@ package com.example.bmicalculator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bmicalculator.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         resultButton.setOnClickListener {
-            val height = heightEditText.text.toString()
-            val weight = weightEditText.text.toString()
+            val height = binding.heightEditText.text.toString()
+            val weight = binding.weightEditText.text.toString()
             val intent = Intent(this,ResultActivity::class.java)
                 .putExtra("height",height)
                 .putExtra("weight",weight)
